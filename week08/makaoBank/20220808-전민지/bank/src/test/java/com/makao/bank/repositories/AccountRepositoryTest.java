3ff7daf3a50624e218f0535b35abfedfe55b5391
Jeon Minji <mjjeon2645@gmail.com>
2022-08-08 11:38:17 +0900

    20220808-강의반복과제-전민지
    20220808-마카오뱅크-잔액조회-전민지

---
+++ week08/makaoBank/20220808-전민지/bank/src/test/java/com/makao/bank/repositories/AccountRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.makao.bank.repositories;
+
+import com.makao.bank.models.Account;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountRepositoryTest {
+  @Test
+  void find() {
+    AccountRepository accountRepository = new AccountRepository();
+
+    Account account = accountRepository.find("1234");
+
+    assertThat(account.identifier()).isEqualTo("1234");
+    assertThat(account.name()).isEqualTo("Ashal");
+    assertThat(account.amount()).isEqualTo(3000);
+  }
+
+  @Test
+  void notFound() {
+    AccountRepository accountRepository = new AccountRepository();
+
+    Account account = accountRepository.find("4321");
+
+    assertThat(account).isNull();
+  }
+}
