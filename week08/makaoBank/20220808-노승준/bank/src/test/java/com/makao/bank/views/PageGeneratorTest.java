ead579fe1d0761ed3087ce20e37e1f6322959644
seungjjun <il1uso@naver.com>
2022-08-08 11:12:29 +0900

    8주차 월요일 주간과제 마카오뱅크-잔액조회

---
+++ week08/makaoBank/20220808-노승준/bank/src/test/java/com/makao/bank/views/PageGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.makao.bank.views;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PageGeneratorTest {
+  @Test
+  void html() {
+    PageGenerator pageGenerator = new PageGenerator() {
+      @Override
+      public String content() {
+        return "Hello, world!";
+      }
+    };
+
+    String html = pageGenerator.html();
+
+    assertThat(html).contains("Hello, world!");
+    assertThat(html).contains("<html");
+    assertThat(html).contains("<a href=\"/\">Home</a>");
+    assertThat(html).contains("<a href=\"/account\">잔액 조회</a>");
+  }
+}
